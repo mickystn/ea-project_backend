b54cf6c1ba501e7d7527642567c23997165883ac
Mickie <mickystn10@gmail.com>
2023-04-19 13:30:45 +0700

    KUY I SUS

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+// Middlewares
+const app = express();
+
+// Routes
+app.get("/", (req,res)=>{
+    res.send("พ่อมึงตาย")
+});
+
+// connection
+app.listen(6969, () => console.log(`Listening to port 6969`));
